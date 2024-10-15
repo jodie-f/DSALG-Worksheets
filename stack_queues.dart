2de80646a6169384ea9932f1d2259745bb882e29
jodie-f <jodiewlfan@gmail.com>
2024-10-15 09:05:07 +0100

    enqueue and dequeue done

--- stack_queues.dart
+++ stack_queues.dart
@@ -1,17 +1,18 @@
 void main() {
-  Queue queue(["1", "2", "3", "4"]);
+  Queue queue = Queue();
   queue.enqueue(6);
+  print(queue.items);
 }
 
 class Queue {
-  List<String> items = [];
+  List<dynamic> items = [];
   int head = 0;
   int tail = -1;
   int queueSize = 15;
 
-  Queue(this.items);
+  Queue();
 
-  void enqueue(String newData) {
+  void enqueue(dynamic newData) {
     print("Enqueue the data $newData");
     if (tail == queueSize - 1) {
       print("Can't insert into a full queue");
@@ -20,5 +21,20 @@ class Queue {
     tail += 1;
     items.insert(tail, newData);
     print("Now the tail index is $tail");
+  }
+
+  void dequeue() {
+    if (tail == -1) {
+      print("Can't delete from an empty queue!!!!!");
+      return;
+    }
+    dynamic removed = items[head];
+    print("Dequeue the data $removed");
+    dynamic temp = head;
+    while (temp < tail) {
+      temp += 1;
+      items[temp - 1] = items[temp];
+    }
+    items.remove(temp);
   }
 }

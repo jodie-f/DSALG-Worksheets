6644dfb1664a8bd1a4593e95ef7a85e5f402f629
jodie-f <jodiewlfan@gmail.com>
2024-10-18 18:28:06 +0100

    insertion and bubble sort, linear search complete. start binary iteration sort

--- searching_sorting.dart
+++ searching_sorting.dart
@@ -1,39 +1,86 @@
 void main() {
-  Sort sort = Sort();
-  print(sort.unsorted);
-  print("Selection sort: ${sort.selection()}");
+  List<int> unsorted = [3, 5, 7, 1, 2, 9, 0];
+
+  print("Unsorted List: $unsorted");
+  print("Selection sort: ${selection(unsorted)}");
+  print("Insertion sort: ${insertion(unsorted)}");
+  print("Bubble sort: ${bubble(unsorted)}");
+
+  print("Search for 5, index: ${linear(unsorted, 5)}");
+  print("Search for 11, index: ${linear(unsorted, 11)}");
 }
 
-class Sort {
-  List<int> unsorted = [3, 5, 7, 1, 2, 9, 0];
-  List<int> sorted = [];
-
-  List<int> selection() {
-    int n = unsorted.length;
-    // going through each element in the unsorted list
-    for (int i = 0; i < n - 1; i++) {
-      int smallestInd = i;
-      // finding the smallest element in the unsorted list
-      for (int j = i + 1; j < n; j++) {
-        if (unsorted[j] < unsorted[smallestInd]) {
-          smallestInd = j;
-        }
-      }
-
-      // swapping the smllest element at i
-      if (smallestInd != i) {
-        int temp = unsorted[i];
-        unsorted[i] = unsorted[smallestInd];
-        unsorted[smallestInd] = temp;
+// Q1
+List<int> selection(List<int> unsorted) {
+  int n = unsorted.length;
+  // going through each element in the unsorted list
+  for (int i = 0; i < n - 1; i++) {
+    int smallestInd = i;
+    // finding the smallest element in the unsorted list
+    for (int j = i + 1; j < n; j++) {
+      if (unsorted[j] < unsorted[smallestInd]) {
+        smallestInd = j;
       }
     }
-    sorted = unsorted;
-    return sorted;
+
+    // swapping the smallest element at i
+    if (smallestInd != i) {
+      int temp = unsorted[i];
+      unsorted[i] = unsorted[smallestInd];
+      unsorted[smallestInd] = temp;
+    }
+  }
+  return unsorted;
+}
+
+// Q2
+List<int> insertion(List<int> unsorted) {
+  int n = unsorted.length;
+  for (int i = 0; i < n; i++) {
+    int key = unsorted[i];
+    int j = i;
+
+    while ((j > 0) && (unsorted[j - 1] > key)) {
+      unsorted[j] = unsorted[j - 1];
+      j--;
+    }
+    unsorted[j] = key;
   }
 
-  void insertion() {}
-
-  void bubble() {}
-
-  String toString() => "Unsorted List: $unsorted";
+  return unsorted;
 }
+
+// Q3
+List<int> bubble(List<int> unsorted) {
+  bool sorted = false;
+  int n = unsorted.length;
+  while (!sorted) {
+    sorted = true;
+    for (int i = 0; i < n - 1; i++) {
+      if (unsorted[i] > unsorted[i + 1]) {
+        sorted = false;
+        int temp = unsorted[i];
+        unsorted[i] = unsorted[i + 1];
+        unsorted[i + 1] = temp;
+      }
+    }
+  }
+
+  return unsorted;
+}
+
+// Q4
+int linear(List<int> list, int target) {
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == target) {
+      return i;
+    }
+  }
+  return -1; // index -1 => target not found
+}
+
+// Q5
+void binaryIter(List<int> unsorted) {}
+
+// Q6
+void binaryRecur(List<int> unsorted) {}
